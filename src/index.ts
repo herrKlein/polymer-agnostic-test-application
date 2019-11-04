efcce7cd85a14d7a43cd2e1fdc1bed9a7ebc24a3
patrick <patrick.deklein@cito.nl>
2019-11-04 23:44:58 +0100

    player

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {LazyElement} from 'qti-components-typescript'
+import {PolymerDefault} from 'qti-components-typescript'
+import {SimpleGreeting} from 'qti-components-typescript'
+
+SimpleGreeting;
+PolymerDefault;
+LazyElement;
+
+customElements.define('polymer-default', PolymerDefault);
